59de4538ce910916681ddb2a0c87784f0e84961e
Jgarette0 <aajiliangarette@gmail.com>
2023-12-18 17:17:03 +0800

    initial commi

---
+++ public/freecodecamp/04-basic.js
@@ -0,0 +1,12 @@
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25)
+    // Only change code above this line
+    return "yes";
+  else {
+    return "no";
+  }
+}
+
+console.log(testLogicalAnd(27));
