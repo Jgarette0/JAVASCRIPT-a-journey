dee4f836b706f5290d2113937adc1ace85a8d9be
Jgarette0 <aajiliangarette@gmail.com>
2023-12-22 01:57:21 +0800

    array for loop

---
+++ public/freecodecamp/15-basics.js
@@ -0,0 +1,11 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
+
+console.log(myArray);
+// You should be using a for loop for this.
+// Passed:myArray should equal [1, 2, 3, 4, 5].
