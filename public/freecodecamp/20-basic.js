b1917f9ed384f054b5ce27dc959a6c23127fe7a2
Jgarette0 <aajiliangarette@gmail.com>
2023-12-24 00:09:21 +0800

    min,max,math floor, math random

--- public/freecodecamp/20-basic.js
+++ public/freecodecamp/20-basic.js
@@ -6,3 +6,10 @@ const all = sum;
 console.log(all);
 console.log(sum);
 console.log(num);
+
+function deym(myMin, myMax) {
+  const boom = Math.floor(Math.random() * (myMin - myMax + 1)) + myMin;
+  return boom;
+}
+
+console.log(deym(4, 6));
