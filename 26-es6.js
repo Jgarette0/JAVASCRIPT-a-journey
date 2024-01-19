1cd4c8650810207232b1160770beb1519f1ac4ac
Jgarette0 <aajiliangarette@gmail.com>
2024-01-19 17:05:20 +0800

    declarative function

--- 26-es6.js
+++ 26-es6.js
@@ -1,1 +1,16 @@
-//hello
+//oncise Declarative Functions with ES6
+
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear: function (newGear) {
+    this.gear = newGear;
+  },
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+// Traditional function expression should not be used.
+// Waiting:setGear should be a declarative function.
+// Waiting:bicycle.setGear(48) should change the gear value to 48.
