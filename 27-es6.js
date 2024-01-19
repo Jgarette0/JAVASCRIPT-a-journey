527cb38ac8b4c45a35538406c98047c12ed8f24b
Jgarette0 <aajiliangarette@gmail.com>
2024-01-19 21:16:08 +0800

    Constructor function and declarative.

---
+++ 27-es6.js
@@ -0,0 +1,16 @@
+//constructor function
+// Only change code below this line
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+// Only change code above this line
+
+const carrot = new Vegetable("carrot");
+console.log(carrot.name); // Should display 'carrot'
+
+// Vegetable should be a class with a defined constructor method.
+// Waiting:The class keyword should be used.
+// Waiting:Vegetable should be able to be instantiated.
+// Waiting:carrot.name should return carrot.
