76daa90456e2213faf1ea7ecec0f95e80405f75e
Jgarette0 <aajiliangarette@gmail.com>
2024-02-03 20:33:01 +0800

    hm

---
+++ 29-regex.js
@@ -0,0 +1,11 @@
+let quoteSample =
+  "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aiu]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+
+// You should find all 25 vowels.
+// Waiting:Your regex vowelRegex should use a character class.
+// Waiting:Your regex vowelRegex should use the global flag.
+// Waiting:Your regex vowelRegex should use the case insensitive flag.
+// Waiting:Your regex should not match any consonants.
+//solved
