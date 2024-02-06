639884f19ce61235f557b8a21dbe74d75f497529
Jgarette0 <aajiliangarette@gmail.com>
2024-02-06 21:05:05 +0800

    smthing

--- 29-regex.js
+++ 29-regex.js
@@ -8,4 +8,3 @@ let result = quoteSample.match(vowelRegex); // Change this line
 // Waiting:Your regex vowelRegex should use the global flag.
 // Waiting:Your regex vowelRegex should use the case insensitive flag.
 // Waiting:Your regex should not match any consonants.
-//solve
