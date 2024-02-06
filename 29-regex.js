2d32f625ee24ab2e1d07e5af0b8771fee95d500c
Jgarette0 <aajiliangarette@gmail.com>
2024-02-07 00:52:31 +0800

    hmm

--- 29-regex.js
+++ 29-regex.js
@@ -8,3 +8,4 @@ let result = quoteSample.match(vowelRegex); // Change this line
 // Waiting:Your regex vowelRegex should use the global flag.
 // Waiting:Your regex vowelRegex should use the case insensitive flag.
 // Waiting:Your regex should not match any consonants.
+//new
