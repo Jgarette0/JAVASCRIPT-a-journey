694fa7df04ceab96b48dc8a38dfaf5d6aea246a8
Jgarette0 <aajiliangarette@gmail.com>
2024-02-18 19:13:28 +0800

    OOP

--- 90-OOP.js
+++ 90-OOP.js
@@ -1,4 +1,6 @@
-function Animal() {}
+function Animal(name) {
+  this.name = name;
+}
 
 Animal.prototype = {
   constructor: Animal,
@@ -8,11 +10,25 @@ Animal.prototype = {
 };
 
 // Only change code below this line
-const animal = new Animal();
+const animal = new Animal("dogo");
 let duck = Object.create(Animal.prototype); // Change this line
 let beagle = Object.create(Animal.prototype); // Change this line
-
+console.log(animal);
 console.log(duck);
 console.log(beagle);
+
+//check if the property exist in constructor Animal
+
+let dsplyHere = [];
+for (let property in animal) {
+  if (animal.hasOwnProperty(property)) {
+    dsplyHere.push(property);
+  }
+}
+console.log(dsplyHere);
+
+//check if animal is instance of Animal then returrn the propery value if it is
+console.log(animal instanceof Animal ? animal.name : `Do't have a property`);
+
 // let animal = new Animal();
 // console.log(animal instanceof Animal);
