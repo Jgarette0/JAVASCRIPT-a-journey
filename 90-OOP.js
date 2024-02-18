42b33460912a1196f216361e8cec738800f46fb6
Jgarette0 <aajiliangarette@gmail.com>
2024-02-18 19:08:21 +0800

    OOP inheritance

--- 90-OOP.js
+++ 90-OOP.js
@@ -8,6 +8,11 @@ Animal.prototype = {
 };
 
 // Only change code below this line
+const animal = new Animal();
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
 
-let duck; // Change this line
-let beagle; // Change this line
+console.log(duck);
+console.log(beagle);
+// let animal = new Animal();
+// console.log(animal instanceof Animal);
