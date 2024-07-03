4d68811fbc6b5ad4ffd47c2288ceebb84915895b
Jgarette0 <aajiliangarette@gmail.com>
2024-07-03 14:43:03 +0800

    Fixed: OOP class and constructor in authentication setting

--- 77-OOP.js
+++ 77-OOP.js
@@ -1,13 +1,48 @@
-class Car {
-  constructor() {
-    this.color = "yellow";
-    this.brand = "McLaren";
-    this.release = 2022;
+class User {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.loggedIn = false;
   }
-  carDetail() {
-    return this.brand + " " + this.color + " " + this.release;
+
+  login(inputPassword) {
+    if (inputPassword === this.password && this.loggedIn) {
+      this.loggedIn = true;
+      console.log(`${this.username} has logged in.`);
+    } else if (!this.loggedIn) {
+      console.log("Please log in first");
+    } else {
+      console.log("invalid password");
+    }
+  }
+  verifyAuth() {
+    if (this.loggedIn) {
+      console.log(`Welcome ${this.username}! here is your email ${this.email}`);
+    } else {
+      console.log("Please Log in first");
+    }
+  }
+  logout() {
+    this.loggedIn = false;
+    console.log(`${this.username} has logged out.`);
   }
 }
 
-const myCar = new Car();
-console.log(myCar.carDetail());
+const user1 = new User("Jgarette0", "Jgarette@gmail.com", "bomtaratarat");
+console.log(user1.verifyAuth());
+console.log(user1.login("bomtaratarat"));
+console.log(user1.logout());
+// class Car {
+//   constructor() {
+//     thn";
+//     this.release = 2022;is.color = "yellow";
+//     this.brand = "McLare
+//   }
+//   carDetail() {
+//     return this.brand + " " + this.color + " " + this.release;
+//   }
+// }
+
+// const myCar = new Car();
+// console.log(myCar.carDetail());s
